b2ba0ff8257fa359b38a4163b71ede2365353da5
phiree <phiree@gmail.com>
2014-01-25 18:00:27 +0800

    tutorial part 5

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+import datetime
 
+from django.test import TestCase
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+from polls.models import Poll
 # Create your tests here.
+class PollMethoedTests(TestCase):
+    def test_was_published_recently_with_future_pull(self):
+        '''
+        was_published_recently should return False for polls
+        whose pub_date is in the future
+        '''
+        future_poll=Poll(pub_date=timezone.now()+datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
+        
+    def test_was_published_recently_with_old_poll(self):
+        future_poll=Poll(pub_date=timezone.now()-datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
+        
+    def test_was_published_recently_with_rencet_poll(self):
+        future_poll=Poll(pub_date=timezone.now()-datetime.timedelta(hours=1))
+        self.assertEqual(future_poll.was_published_recently(), True)
+
+def create_poll(question,days):
+    '''create a poll with the given question and published the given number 
+    of days offset to new(negative for polls published in the past,positive
+    for the polls tat have yet to be published'''   
+    return Poll.objects.create(question=question
+                               ,pub_date=timezone.now()+datetime.timedelta(days=days))
+        
+class PollViewTests(TestCase):
+    def test_indexView_with_no_polls(self):
+        '''If no polls exist, an appropriate message should be displayed'''      
+        response=self.client.get(reverse("polls:index"))
+        print (response)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No")
+        self.assertQuerysetEqual(response.context['latest_poll_list'], [])
+    def test_indexView_with_a_future_poll(self):
+        poll=create_poll('q1',30)
+        response=self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context['latest_poll_list'],[])
+    def test_indexView_with_a_past_poll(self):
+        poll=create_poll('q1',-30)
+        response=self.client.get(reverse("polls:index"))
+        self.assertEqual(list(response.context['latest_poll_list']),[poll])
+
+class PollDetailtests(TestCase):
+    def test_detail_view_with_a_future_poll(self):
+        future_poll=create_poll('FP',20)
+        response=self.client.get(reverse("polls:detail",args=(future_poll.id,)))
+        self.assertEqual(response.status_code,404)
+    def test_detail_view_with_a_recent_poll(self):
+        recent_poll=create_poll('RP',-1)
+        print(recent_poll.id)
+        response=self.client.get(reverse("polls:detail",args=(recent_poll.id,)))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,recent_poll)
+        
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,28 @@
+from django.utils import timezone
 from django.shortcuts import render,get_object_or_404
-from polls.models import Poll
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import  reverse
+from django.views import generic
+from polls.models import Poll,Choice
+
+
+class IndexView(generic.ListView):
+    template_name='polls/index.html'
+    context_object_name='latest_poll_list'
+    def get_queryset(self):
+        return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model=Poll
+    template_name='polls/detail.html'
+    '''
+    Excludes any polls that are't published yet'''
+    def get_queryset(self):
+        return Poll.objects.filter(pub_date__lte=timezone.now())
+    
+class ResultsView(generic.DetailView):
+    model=Poll
+    template_name='polls/results.html'
 
 def index(request):
     latest_poll_list=Poll.objects.order_by('-pub_date')[:5]
@@ -12,4 +35,19 @@ def detail(request,poll_id):
    
     context={'poll':poll}
     return render(request,'polls/detail.html',context)
+
+def results(request,poll_id):
+    poll=get_object_or_404(Poll,pk=poll_id)
+    return render(request,'polls/results.html',{'poll':poll})
+
+def vote(request,poll_id):
+    p=get_object_or_404(Poll,pk=poll_id)
+    try:
+        selected_choice=p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{'poll':p,'error_message':"You didn't select a choice."})
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
 # Create your views here.
