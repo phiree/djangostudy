cc71401cd1014a5218331fa32d0c6acf425e1785
phiree <phiree@gmail.com>
2014-01-27 00:24:26 +0800

    copy app polls to study reuse

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from mysite.views import home
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    ('^$',home),
     url(r'^polls/',include('polls.urls',namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ mysite/polls/views.py
@@ -0,0 +1,53 @@
+from django.utils import timezone
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import  reverse
+from django.views import generic
+from polls.models import Poll,Choice
+
+
+class IndexView(generic.ListView):
+    template_name='polls/index.html'
+    context_object_name='latest_poll_list'
+    def get_queryset(self):
+        return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model=Poll
+    template_name='polls/detail.html'
+    '''
+    Excludes any polls that are't published yet'''
+    def get_queryset(self):
+        return Poll.objects.filter(pub_date__lte=timezone.now())
+    
+class ResultsView(generic.DetailView):
+    model=Poll
+    template_name='polls/results.html'
+
+def index(request):
+    latest_poll_list=Poll.objects.order_by('-pub_date')[:5]
+    context={'latest_poll_list':latest_poll_list}
+    return render(request,'polls/index.html',context)
+
+def detail(request,poll_id):
+   
+    poll=get_object_or_404(Poll,pk=poll_id)
+   
+    context={'poll':poll}
+    return render(request,'polls/detail.html',context)
+
+def results(request,poll_id):
+    poll=get_object_or_404(Poll,pk=poll_id)
+    return render(request,'polls/results.html',{'poll':poll})
+
+def vote(request,poll_id):
+    p=get_object_or_404(Poll,pk=poll_id)
+    try:
+        selected_choice=p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{'poll':p,'error_message':"You didn't select a choice."})
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
+# Create your views here.
